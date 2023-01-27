a215b5f7db284fc0ed171b9b33512fe5f213ec80
Hilora Dewett <h.dewett@outlook.com>
2023-01-27 02:54:32 -0500

    Add basic HTTP auth to admin pages.

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,13 @@
 class Admin::DashboardController < ApplicationController
-  def show
+  http_basic_authenticate_with name: "dhh", password: "secret", except: :index
+
+  def index
+    render plain: "Everyone can see me!"
   end
+
+  def edit
+    render plain: "I'm only accessible if you know the password"
+  end
+# def show
+  # end
 end
