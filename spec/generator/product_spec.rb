78d4f1a8e097f8e4d0467a57f6183ebfd84dcb2d
Hilora Dewett <h.dewett@outlook.com>
2023-01-31 19:53:20 -0500

    add product spec tests

---
+++ spec/generator/product_spec.rb
@@ -0,0 +1,57 @@
+require 'rails_helper'
+
+RSpec.describe Product, type: :model do
+
+#####################
+
+#####################
+
+  describe 'Validations' do
+    # validation tests/examples here
+
+    it 'should save a new product with all fields present' do
+      @category = Category.new(name: 'Grass')
+      @product = Product.new(name: 'plant', price: '10', quantity: 5, category: @category)
+      @product.save
+
+      expect(@product.save).to be true
+    end
+
+    it 'should be invalid without a name' do
+      @category = Category.new(name: 'Grass')
+      @product = Product.new(name: nil, price_cents: '10', quantity: 5, category: @category)
+      @product.save
+
+      expect(@product.errors.full_messages).to include("Name can't be blank")
+    end
+
+    it 'should be invalid without a price' do
+      @category = Category.new(name: 'Grass')
+      @product = Product.new(name: 'plant', price_cents: nil, quantity: '5', category: @category)
+      @product.save
+
+      expect(@product.errors.full_messages).to include("Price can't be blank")
+    end
+
+    it 'should be invalid without a quantity' do
+      @category = Category.new(name: 'Grass')
+      @product = Product.new(name: 'plant', price_cents: '10', quantity: nil, category: @category)
+      @product.save
+
+      expect(@product.errors.full_messages).to include("Quantity can't be blank")
+    end
+
+    it 'should be invalid without a category' do
+      @category = Category.new(name: 'Grass')
+      @product = Product.new(name: 'plant', price_cents: '10', quantity: '5', category: nil)
+      @product.save
+
+      expect(@product.errors.full_messages).to include("Category can't be blank")
+    end
+
+
+
+
+
+  end
+end
